6fb4705b45402300b268b89eb0257b6f75e8fd94
Mason Waterworth <hello@masonwaterworth.com>
2020-10-28 21:45:59 -0400

    💄 ✨ Search by colour and styling added by Cleo

--- scripts/index.js
+++ scripts/index.js
@@ -1,83 +1,101 @@
 const apikey = '?apikey=eedd9666-26c5-4420-9da8-6c05c5771dac';
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+let imgArr = [];
+
+function popArray() {
+  for (let i = 0; i < 50; i++) {
+    const populateArray = axios
+      .get(
+        'https://api.harvardartmuseums.org/object/' + apikey + '&page=' + [i]
+      )
+      .then((result) => {
+        for (let i = 0; i < 10; i++) {
+          if (!imgArr.includes(result.data.records[i].id)) {
+            imgArr.push(result.data.records[i].id);
+          }
+        }
+      })
+      .catch((error) => {});
+  }
 }
 
-let imgArr = [];
 function getImages() {
-  for (let i = 0; i < 100; i++) {
-    imgArr.push(getRandomInt(1000000));
-  }
-  //console.log(imgArr);
-
   imgArr.forEach((index) => {
     const getdata = axios
       .get('https://api.harvardartmuseums.org/object/' + index + apikey)
       .then((result) => {
-        let body = document.querySelector('.display_data');
-        let wrapper = document.createElement('div');
+        let dataContainer = document.querySelector('.display-data');
+        let itemCard = document.createElement('div');
+        itemCard.classList.add('display-data__card');
+        let itemLink = document.createElement('a');
+        itemLink.classList.add('display-data__link');
+        itemLink.href = result.data.images[0].baseimageurl;
+        itemCard.appendChild(itemLink);
         let img = document.createElement('img');
+        img.classList.add('display-data__img');
         img.src = result.data.images[0].baseimageurl;
-        wrapper.appendChild(img);
-        console.log(result.data.images[0].baseimageurl);
-        body.appendChild(wrapper);
+        itemLink.appendChild(img);
+        let itemTitle = document.createElement('p');
+        itemTitle.classList.add('display-data__title');
+        itemTitle.innerText = result.data.title;
+        itemCard.appendChild(itemTitle);
+        let itemArtist = document.createElement('p');
+        itemArtist.classList.add('display-data__artist');
+        itemArtist.innerText = '-' + result.data.people[0].name;
+        itemCard.appendChild(itemArtist);
+
+        dataContainer.appendChild(itemCard);
       })
       .catch((error) => {});
   });
 }
+
+popArray();
+
+getImages();
 
 const formEl = document.getElementById('searchForm');
 
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const searchQuery = e.target.searchQuery.value;
-  // console.log(searchQuery);
-  let body = document.querySelector('.display_data');
-  body.innerHTML = '';
-  let wrapper = document.createElement('div');
+  let dataContainer = document.querySelector('.display-data');
+  dataContainer.innerHTML = '';
+
+  if (e.target.searchQuery.value == '') {
+    getImages();
+  }
 
   imgArr.forEach((index) => {
     const getdata = axios
       .get('https://api.harvardartmuseums.org/object/' + index + apikey)
       .then((result) => {
-        console.log(result.data.colors);
-
         let dominantCol = result.data.colors[0].hue;
-        //console.log(dominantCol);
         if (dominantCol == e.target.searchQuery.value) {
+          let dataContainer = document.querySelector('.display-data');
+          let itemCard = document.createElement('div');
+          itemCard.classList.add('display-data__card');
+          let itemLink = document.createElement('a');
+          itemLink.classList.add('display-data__link');
+          itemLink.href = result.data.images[0].baseimageurl;
+          itemCard.appendChild(itemLink);
           let img = document.createElement('img');
+          img.classList.add('display-data__img');
           img.src = result.data.images[0].baseimageurl;
-          wrapper.appendChild(img);
+          itemLink.appendChild(img);
+          let itemTitle = document.createElement('p');
+          itemTitle.classList.add('display-data__title');
+          itemTitle.innerText = result.data.title;
+          itemCard.appendChild(itemTitle);
+          let itemArtist = document.createElement('p');
+          itemArtist.classList.add('display-data__artist');
+          itemArtist.innerText = '-' + result.data.people[0].name;
+          itemCard.appendChild(itemArtist);
 
-          console.log('Color matches search');
-          console.log(result.data.images[0].baseimageurl);
+          dataContainer.appendChild(itemCard);
         }
-        console.log(wrapper);
-        body.appendChild(wrapper);
-
-        // wrapper.innerText = result.data;
-        // console.log(result.data.images[0].baseimageurl);
+        // dataContainer.appendChild(itemCard);
       })
       .catch((error) => {});
   });
 });
-
-console.log(getImages());
-
-// Search by color
-
-// We have an populated.
-
-// Display all images
-
-// Take in a search query ($Colour)
-
-// console.log(result.data.color[0].hex)
-
-// Check if display image contains $colour
-//  if true, keep in array
-// if false remove from array.
-
-// Display new array of images
