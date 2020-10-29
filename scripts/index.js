62b4011685f7c7e39e9b72418cf565770b19812a
Mason Waterworth <hello@masonwaterworth.com>
2020-10-28 23:56:26 -0400

    💄 Changed card layout to reflect a museum label

--- scripts/index.js
+++ scripts/index.js
@@ -25,8 +25,11 @@ function getImages() {
       .get('https://api.harvardartmuseums.org/object/' + index + apikey)
       .then((result) => {
         let dataContainer = document.querySelector('.display-data');
+        // Item -- Card Element
         let itemCard = document.createElement('div');
         itemCard.classList.add('display-data__card');
+
+        // Item - Image Link
         let itemLink = document.createElement('a');
         itemLink.classList.add('display-data__link');
         itemLink.href = result.data.images[0].baseimageurl;
@@ -35,14 +38,41 @@ function getImages() {
         img.classList.add('display-data__img');
         img.src = result.data.images[0].baseimageurl;
         itemLink.appendChild(img);
-        let itemTitle = document.createElement('p');
-        itemTitle.classList.add('display-data__title');
-        itemTitle.innerText = result.data.title;
-        itemCard.appendChild(itemTitle);
+
+        // Item -- Artist
         let itemArtist = document.createElement('p');
         itemArtist.classList.add('display-data__artist');
-        itemArtist.innerText = '-' + result.data.people[0].name;
+        itemArtist.innerText = result.data.people[0].name;
+
+        // Item -- Artist Life
+        let artistLife = document.createElement('p');
+        artistLife.classList.add('display-data__item-date');
+        if (result.data.people[0].displaydate == null) {
+          artistLife.innerText = 'Unknown';
+        } else {
+          artistLife.innerText = '(' + result.data.people[0].displaydate + ')';
+        }
+
         itemCard.appendChild(itemArtist);
+        itemCard.appendChild(artistLife);
+
+        // Item  -- Title
+        let itemTitle = document.createElement('p');
+        itemTitle.classList.add('display-data__title');
+        itemTitle.innerText = result.data.title + ', ' + result.data.dated;
+        itemCard.appendChild(itemTitle);
+
+        // Item -- Medium
+        let itemMedium = document.createElement('p');
+        itemMedium.classList.add('display-data__medium');
+        itemMedium.innerText = result.data.medium;
+        itemCard.appendChild(itemMedium);
+
+        // Item -- Credit Line
+        let itemCredit = document.createElement('p');
+        itemCredit.classList.add('display-data__credit');
+        itemCredit.innerText = result.data.creditline;
+        itemCard.appendChild(itemCredit);
 
         dataContainer.appendChild(itemCard);
       })
@@ -71,8 +101,11 @@ formEl.addEventListener('submit', (e) => {
         let dominantCol = result.data.colors[0].hue;
         if (dominantCol == e.target.searchQuery.value) {
           let dataContainer = document.querySelector('.display-data');
+          // Item -- Card Element
           let itemCard = document.createElement('div');
           itemCard.classList.add('display-data__card');
+
+          // Item - Image Link
           let itemLink = document.createElement('a');
           itemLink.classList.add('display-data__link');
           itemLink.href = result.data.images[0].baseimageurl;
@@ -81,14 +114,42 @@ formEl.addEventListener('submit', (e) => {
           img.classList.add('display-data__img');
           img.src = result.data.images[0].baseimageurl;
           itemLink.appendChild(img);
-          let itemTitle = document.createElement('p');
-          itemTitle.classList.add('display-data__title');
-          itemTitle.innerText = result.data.title;
-          itemCard.appendChild(itemTitle);
+
+          // Item -- Artist
           let itemArtist = document.createElement('p');
           itemArtist.classList.add('display-data__artist');
-          itemArtist.innerText = '-' + result.data.people[0].name;
+          itemArtist.innerText = result.data.people[0].name;
+
+          // Item -- Artist Life
+          let artistLife = document.createElement('p');
+          artistLife.classList.add('display-data__item-date');
+          if (result.data.people[0].displaydate == null) {
+            artistLife.innerText = 'Unknown';
+          } else {
+            artistLife.innerText =
+              '(' + result.data.people[0].displaydate + ')';
+          }
+
           itemCard.appendChild(itemArtist);
+          itemCard.appendChild(artistLife);
+
+          // Item  -- Title
+          let itemTitle = document.createElement('p');
+          itemTitle.classList.add('display-data__title');
+          itemTitle.innerText = result.data.title + ', ' + result.data.dated;
+          itemCard.appendChild(itemTitle);
+
+          // Item -- Medium
+          let itemMedium = document.createElement('p');
+          itemMedium.classList.add('display-data__medium');
+          itemMedium.innerText = result.data.medium;
+          itemCard.appendChild(itemMedium);
+
+          // Item -- Credit Line
+          let itemCredit = document.createElement('p');
+          itemCredit.classList.add('display-data__credit');
+          itemCredit.innerText = result.data.creditline;
+          itemCard.appendChild(itemCredit);
 
           dataContainer.appendChild(itemCard);
         }
@@ -97,3 +158,9 @@ formEl.addEventListener('submit', (e) => {
       .catch((error) => {});
   });
 });
+
+// Card layout to reflect Museum object label
+// result.data.people[0].name + "("result.data.people[0].displaydate+")";
+// result.data.title + ", " + result.data.dated;
+// result.data.medium
+// result.data.creditline
